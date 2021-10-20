5ac85b40600fbfdfa61e2994db5f510a01b72b02
Carlos Bordalo <carlosvitor6@hotmail.com>
2021-10-20 03:11:29 -0300

    Created appointment table

---
+++ src/database/migrations/1634709049326-CreateAppointments.ts
@@ -0,0 +1,46 @@
+import { id } from "date-fns/locale";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateAppointments1634709049326 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'appointments',
+          columns: [
+            {
+              name: 'id',
+              type: 'varchar',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+              isNullable: false,
+            },
+            {
+              name: 'date',
+              type: 'timestamp with time zone',
+              isNullable: false,
+            },
+            {
+              name: 'email',
+              type: 'varchar',
+              isNullable: false,
+            },
+            {
+              name: 'phone',
+              type: 'varchar',
+              isNullable: false,
+            }
+          ]
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('appointments');
+    }
+
+}
