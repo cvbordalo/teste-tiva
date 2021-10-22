ea327d60ef6832e96731b7b54677b9fb863f7d6a
Carlos Bordalo <carlosvitor6@hotmail.com>
2021-10-22 00:52:53 -0300

    Generated JWT token

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '71ff5bee131836553a557b25b779b34a',
+    expiresIn: '1d',
+  }
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
 import { sign, verify } from 'jsonwebtoken'
+import authConfig from '../config/auth';
 
 import User from '../models/User'
 
@@ -31,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination')
     }
 
-    const token = sign({}, '71ff5bee131836553a557b25b779b34a', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
